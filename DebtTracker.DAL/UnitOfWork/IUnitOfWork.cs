a6231ab653e662538a4d95656a62eb993cc21dcf
Martin Zelenák <93610343+MartinTheKiller@users.noreply.github.com>
2023-06-22 21:09:14 +0200

    Added UnitOfWork to DAL

---
+++ DebtTracker.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using CubiTracker.DAL.Repositories;
+using DebtTracker.DAL.Entities;
+using DebtTracker.DAL.Mappers;
+
+namespace DebtTracker.DAL.UnitOfWork;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    IRepository<TEntity> GetRepository<TEntity, TEntityMapper>()
+        where TEntity : class, IEntity
+        where TEntityMapper : IEntityMapper<TEntity>, new();
+    Task CommitAsync();
+}

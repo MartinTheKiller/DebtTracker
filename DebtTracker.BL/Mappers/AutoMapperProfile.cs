719cc0f5344757895087a63cc27fd28209530181
Martin Zelenák <93610343+MartinTheKiller@users.noreply.github.com>
2023-06-27 15:21:08 +0200

    Fixed model to entity nav property mapping
    When mapping model to entity, navigational properties are now not mapped

--- DebtTracker.BL/Mappers/AutoMapperProfile.cs
+++ DebtTracker.BL/Mappers/AutoMapperProfile.cs
@@ -8,15 +8,29 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        CreateMap<DebtEntity, DebtDetailModel>().ReverseMap();
+        CreateMap<DebtEntity, DebtDetailModel>()
+            .ForMember(m => m.Creditor, expr => expr.Ignore())
+            .ForMember(m => m.Debtor, expr => expr.Ignore())
+            .ForMember(m => m.Group, expr => expr.Ignore())
+            .ReverseMap();
         CreateMap<DebtEntity, DebtListModel>();
 
-        CreateMap<GroupEntity, GroupDetailModel>().ReverseMap();
+        CreateMap<GroupEntity, GroupDetailModel>()
+            .ForMember(m => m.Debts, expr => expr.Ignore())
+            .ForMember(m => m.Users, expr => expr.Ignore())
+            .ReverseMap();
         CreateMap<GroupEntity, GroupListModel>();
 
-        CreateMap<RegisteredGroupEntity, RegisteredGroupModel>().ReverseMap();
+        CreateMap<RegisteredGroupEntity, RegisteredGroupModel>()
+            .ForMember(m => m.Group, expr => expr.Ignore())
+            .ForMember(m => m.User, expr => expr.Ignore())
+            .ReverseMap();
 
-        CreateMap<UserEntity, UserDetailModel>().ReverseMap();
+        CreateMap<UserEntity, UserDetailModel>()
+            .ForMember(m => m.LentDebts, expr => expr.Ignore())
+            .ForMember(m => m.OwesDebts, expr => expr.Ignore())
+            .ForMember(m => m.Groups, expr => expr.Ignore())
+            .ReverseMap();
         CreateMap<UserEntity, UserListModel>();
     }
 }
